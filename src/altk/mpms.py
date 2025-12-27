ebdf8d22c028497509c4f1bcd84fa1129d4b3559
LiGuang <guangli25@outlook.com>
2025-12-27 15:26:49 +0900

    feat(mpms.py): Add function to read mpms data to numpy.ndarray format.

--- src/altk/mpms.py
+++ src/altk/mpms.py
@@ -2,7 +2,10 @@ import pandas as pd
 from pandas import DataFrame
 from matplotlib.axes import Axes
 
+import numpy as np
+
 import logging
+from typing import Union, Literal
 
 from altk.utils._exceptions import DataFileInvalid
 
@@ -13,14 +16,15 @@ COL_H = "Field (Oe)"
 COL_M = "Long Moment (emu)"
 
 
-def read_mpms_data(file: str) -> DataFrame:
+def read_mpms_data_to_df(file: str) -> DataFrame:
     """Read .dat datafile from mpms.
 
     Args:
         file (str): the data file
 
     Returns:
-        pd.DataFrame: Pandas Dataframe with data.
+        Union[DataFrame,np.ndarray]: Numpy array or Pandas Dataframe with data.
+            The return type is assigned with parameter.
             Useful columns:
                 Field (Oe)
                 Temperature (K)
@@ -30,16 +34,36 @@ def read_mpms_data(file: str) -> DataFrame:
         FileNotFoundError: File of the given file path is not found.
         StartPositionNotFound: Data start line does not exist in this file.
     """
-    logger.info(f"Reading from \"{file}\".")
+    logger.info(f'Reading from "{file}".')
     with open(file, "r") as f:
         for i, line in enumerate(f):
             if line.strip() == "[Data]":
                 data_start = i + 1
                 break
         else:
-            raise DataFileInvalid("Data start position \"[Data]\" not found.")
+            raise DataFileInvalid('Data start position "[Data]" not found.')
     df = pd.read_csv(file, skiprows=data_start)
     return df
+
+
+def read_mpms_data_to_np(file: str) -> np.ndarray:
+    """Read mpms data to numpy.ndarray
+
+    Args:
+        file (str): The data file
+
+    Returns:
+        np.ndarray: The transposed data in ndarray.
+        
+        Line assignment as follows
+            0: Field, in Oe
+            1: Temperature, in K
+            2: Long Moment(Magnetisation), in emu.
+    """
+    df_data = read_mpms_data_to_df(file)
+    data = df_data[["Field (Oe)", "Temperature (K)", "Long Moment (emu)"]].to_numpy().T
+    return data
+
 
 def plot_MT(ax: Axes, df: DataFrame, **kwargs):
     """Plot magnetization (M) - temperature (T).
@@ -51,6 +75,7 @@ def plot_MT(ax: Axes, df: DataFrame, **kwargs):
     ax.plot(df[COL_T], df[COL_M], **kwargs)
     ax.set_xlabel("T (K)")
     ax.set_ylabel("M (emu)")
+
 
 def plot_MH(ax: Axes, df: DataFrame, **kwargs):
     """Plot magnetization (M) - magnetic field (H).
