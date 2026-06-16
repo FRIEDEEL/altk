01243d8ade2d9c16f31aa45b3be3e5760e1cc505
LiGuang <guangli25@outlook.com>
2026-06-16 18:42:57 +0900

    wip(scmag): framework for scmag data class and sample class.

--- src/altk/scmag.py
+++ src/altk/scmag.py
@@ -6,15 +6,107 @@ from matplotlib.axes import Axes
 import numpy as np
 
 import logging
-from typing import Union, Literal
+from typing import Union, Literal, Mapping
 
 from altk.utils._exceptions import DataFileInvalid
+from altk.typing.path import DataFile
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-def read_scmag_data_to_df(file: str):
+COL_T = "Temperature (K)"
+COL_I = "I (A)"
+COL_P = "P (C)"
+COL_TIME = "Time (s)"
+
+
+class ScmagSample:
+    def __init__(
+        self, electrode_area: float | None = None, thickness: float | None = None
+    ) -> None:
+        self.electrode_area = electrode_area
+        self.thickness = thickness
+        pass
+
+    @property
+    def electrode_area(self):
+        if self._electrode_area is None:
+            raise ValueError("Empty electrode area. Please set value.")
+        return self._electrode_area
+
+    @electrode_area.setter
+    def electrode_area(self, value: float | None):
+        if value is not None and value <= 0:
+            raise ValueError(f"Electrode area should be larger than 0. Got {value}")
+        else:
+            self._electrode_area = value
+        pass
+
+    @property
+    def thickness(self):
+        if self._thickness is None:
+            raise ValueError("Empty thickness value. Please set value.")
+        return self._thickness
+
+    @thickness.setter
+    def thickness(self, value: float | None):
+        if value is not None and value <= 0:
+            raise ValueError(f"Sample thickness should be larger than 0. Got {value}")
+        else:
+            self._thickness = value
+
+    # aliases
+    @property
+    def A(self):
+        return self.electrode_area
+
+    @property
+    def d(self):
+        return self.thickness
+
+
+class ScmagData:
+    def __init__(
+        self,
+        data: DataFrame,
+        sample: ScmagSample | None = None,
+    ) -> None:
+        self._data = data
+
+    @classmethod
+    def from_file(cls, file: DataFile):
+        data = read_scmag_data_to_df(file)
+        return cls(data=data)
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def temperature(self):
+        return self.data[COL_T]
+
+    @property
+    def time(self):
+        return self.data[COL_TIME]
+
+    @property
+    def current(self):
+        return self.data[COL_I]
+
+    @property
+    def charge(self):
+        return self.data[COL_P]
+
+    # aliases
+    ...
+
+    def set_sample(self, sample: ScmagSample):
+        pass
+
+
+def read_scmag_data_to_df(file: DataFile):
     """Read .dat datafile from scmag datafile (.dat). # TODO: change this
 
     Args:
